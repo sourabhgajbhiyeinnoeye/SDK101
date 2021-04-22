35984952cd374b81959a80a369fbb06d4159e4f8
SourabhGajbhiye <sourabh.gajbhiye@innoeye.com>
2021-04-22 18:32:51 +0530

    unzipped

---
+++ SDK101.framework/Headers/SDK101.h
@@ -0,0 +1,18 @@
+//
+//  SDK101.h
+//  SDK101
+//
+//  Created by Sourabh's MacBookPro on 22/04/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SDK101.
+FOUNDATION_EXPORT double SDK101VersionNumber;
+
+//! Project version string for SDK101.
+FOUNDATION_EXPORT const unsigned char SDK101VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SDK101/PublicHeader.h>
+
+
