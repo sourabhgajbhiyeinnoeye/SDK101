0dfb1f5a2f94f4bc257afc35bb8739382bc9388f
SourabhGajbhiye <sourabh.gajbhiye@innoeye.com>
2021-04-22 17:32:38 +0530

    initial commit

---
+++ SGFDemo.framework/Headers/SGFDemo.h
@@ -0,0 +1,18 @@
+//
+//  SGFDemo.h
+//  SGFDemo
+//
+//  Created by Sourabh's MacBookPro on 22/04/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SGFDemo.
+FOUNDATION_EXPORT double SGFDemoVersionNumber;
+
+//! Project version string for SGFDemo.
+FOUNDATION_EXPORT const unsigned char SGFDemoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SGFDemo/PublicHeader.h>
+
+
